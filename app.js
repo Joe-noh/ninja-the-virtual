273d212108178e57d26a961341afac435a75568a
Joe-noh <goflb.jh@gmail.com>
2018-06-12 00:14:58 +0900

    display products

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
+var imageRouter = require('./routes/image');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/image', imageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const https = require('https');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const token = process.env.SUZURI_TOKEN;
+  const opts = {
+    protocol: 'https:',
+    host: 'suzuri.jp',
+    path: `/api/v1/products?limit=50`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  https.get(opts, response => {
+    let body = '';
+    response.setEncoding('utf8');
+
+    response.on('data', (chunk) => { body += chunk; });
+
+    response.on('end', (_) => {
+      const json = JSON.parse(body);
+
+      const products = json.products.map(product => {
+        return {...product, sampleImageUrl: `/image?url=${encodeURIComponent(product.sampleImageUrl)}`}
+      });
+      res.render('index', {products});
+    });
+  });
 });
 
 module.exports = router;
